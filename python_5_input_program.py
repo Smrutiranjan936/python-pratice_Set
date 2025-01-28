b78f9a0338c880e6970eb0ae1c6b9537964420e3
Smrutiranjan Mallick <149951746+Smrutiranjan936@users.noreply.github.com>
2025-01-28 15:59:41 +0530

    Create python_5_input_program.py

---
+++ python_5_input_program.py
@@ -0,0 +1,16 @@
+#in console we can give input as:   Smrutiranjan
+a=input("Enter your name: ")
+print("My name is:",a);
+#Ans-  My name is:Smrutiranjan
+
+____________________________________________
+
+
+#in console we can give input as:  x=5,y=42
+X=input("Enter your 1st number:");
+y=print("Enter your 2nd number:");
+print(x + y);
+#Ans- 542               """Because the input function is acept only String praporty"""
+print(int(x) + int(y));
+#Ans- 47               """Because we can fast convert these data type String to int"""
+
