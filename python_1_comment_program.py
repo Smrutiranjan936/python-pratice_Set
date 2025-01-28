f3504a96151073d2f4fa7d390ee06e067670e41d
Smrutiranjan Mallick <149951746+Smrutiranjan936@users.noreply.github.com>
2025-01-28 13:17:31 +0530

    Create python_1_comment_program.py

---
+++ python_1_comment_program.py
@@ -0,0 +1,10 @@
+#single line comment in python
+
+"""multiline comment 
+like this -1=i am Smrutiranjan
+2=i am 23 years old
+3=i am a freelancer """
+
+''' this one also multiline comment
+like this-1= i am smrutiranjan
+2=my hobbies are traveling , design, art, book readingetc'''
