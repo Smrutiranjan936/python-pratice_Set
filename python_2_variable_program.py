22144483024ec3e43a983b85182d38c6bc55e996
Smrutiranjan Mallick <149951746+Smrutiranjan936@users.noreply.github.com>
2025-01-28 13:35:08 +0530

    Create  python_2_variable_program.py

---
+++ python_2_variable_program.py
@@ -0,0 +1,18 @@
+# variables- contains the value of variavle
+
+a=56;
+b=48;
+c=a+b;
+d="Rusty";
+e="Nails";
+
+print(a);
+print(c);
+print(a+b);
+print(a*b);
+print(a/b);
+print(a-b);
+print(d+e);
+print(a+e);
+
+
