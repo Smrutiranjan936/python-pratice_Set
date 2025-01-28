affd25ee8784482665d21656624f0f9f9741090d
Smrutiranjan Mallick <149951746+Smrutiranjan936@users.noreply.github.com>
2025-01-28 15:32:17 +0530

    Create python_3_operators_program.py

---
+++ python_3_operators_program.py
@@ -0,0 +1,14 @@
+print(15+26);   #Addition
+#Ans-41
+print(45-25);   #Subtraction
+#Ans-20
+print(7*8);     #Multiplication
+#Ans-56
+print(15/3);    #Division
+#Ans-5
+print(15//6);   #Floor Division
+#Ans-2
+print(5%3);     #Modulus
+#Ans-2
+print(3**2);    #Exponential
+#3 to the power 2
