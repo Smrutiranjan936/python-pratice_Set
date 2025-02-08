a86872846524cac219869eaa3d8698c9d51d6463
Smrutiranjan Mallick <149951746+Smrutiranjan936@users.noreply.github.com>
2025-02-08 06:47:02 +0530

    Create python_12_while-loop_program.py

---
+++ python_12_while-loop_program.py
@@ -0,0 +1,74 @@
+a=0;
+while(a<8):
+    print(a)
+    a=a+1;
+#Output
+"""
+0
+1
+2
+3
+4
+5
+6
+7
+"""
+
+
+b=int(input("Enter a Number:"))
+while(b<50):
+    print(b)
+    b=b+1
+print("you chose wrong number")
+#Output
+"""
+Enter a Number:35
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+you chose wrong number
+"""
+
+
+#Decriment
+
+c=int(input("Enter your Number:"))
+while(c>5):
+    print(c)
+    c=c-1
+print("stop now")
+#Output
+
+"""
+Enter your Number:10
+10
+9
+8
+7
+6
+stop now
+"""
+
+d=int(input("Enter your Number:"))
+while(d>10):
+    print(d)
+    d=d-1
+else:
+    print("stop now")
+#Output
+"""
+Enter your Number:5
+stop now
+"""
