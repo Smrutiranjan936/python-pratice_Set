590f98f12e54d009b21c83817b41b875e4eec420
Smrutiranjan Mallick <149951746+Smrutiranjan936@users.noreply.github.com>
2025-02-08 20:43:53 +0530

    Create python_13_break-continue-statement_program.py

---
+++ python_13_break-continue-statement_program.py
@@ -0,0 +1,32 @@
+#Break statement
+for a in range(10):
+    if(a==5):
+        break
+    print(a)
+#Output
+"""
+0
+1
+2
+3
+4
+"""
+
+
+#continue statement
+for b in range(10):
+    if(b==6):
+        continue
+    print(b)
+#Output
+"""
+0
+1
+2
+3
+4
+5
+7
+8
+9
+"""
