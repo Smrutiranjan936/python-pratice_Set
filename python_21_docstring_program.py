581ad65417bc082be1e599dc7142185d6c78dabb
Smrutiranjan Mallick <149951746+Smrutiranjan936@users.noreply.github.com>
2025-02-27 15:38:10 +0530

    Create python_21_docstring_program.py

---
+++ python_21_docstring_program.py
@@ -0,0 +1,9 @@
+def square(n):
+    # print(n)                    #if we can use print after column then it can't be shon docstring it given output only none
+    '''Takes in a number n, return the square of n'''
+    print(n**2)
+square(5)
+print(square.__doc__)
+
+
+"""Zen of python"""
